03e1f97fccb731f62121eb97306387b0c15afdf5
Phanthip <phanthiphyphuththwngs@gmail.com>
2024-10-11 13:06:40 +0700

    Lap 6 .c

--- Lap 6 .c
+++ Lap 6 .c
@@ -1,41 +1,16 @@
 #include <stdio.h>
 
 int main() {
-    
-    int matrix4x4[4][4] = {
-        {1, 0, 0, 0},
-        {0, 1, 0, 0},
-        {0, 0, 1, 0},
-        {0, 0, 0, 1}
-    };
+    int N;
 
+    // รับค่าตัวเลขจากผู้ใช้
+    printf("Enter value: ");
+    scanf("%d", &N);
     
-    int matrix5x5[5][5] = {
-        {0, 0, 0, 0, 1},
-        {0, 0, 0, 1, 0},
-        {0, 0, 1, 0, 0},
-        {0, 1, 0, 0, 0},
-        {1, 0, 0, 0, 0}
-    };
-
-    printf( "4x4 Identity Matrix:\n" ) ;
-    for ( int i = 0; i < 4; i++ ) {
-        for ( int j = 0; j < 4; j++ ) {
-            printf( "%d ", matrix4x4[i][j] ) ;
+    for (int i = N; i >= 0; i--) {
+        if (i % 2 == 0) {
+            printf("%d ", i);
         }
-        printf( "\n" ) ;
     }
-
-    printf( "\n" ) ; 
-
-    
-    printf( "5x5 Matrix:\n" ) ;
-    for ( int i = 0; i < 5; i++ ) {
-        for ( int j = 0; j < 5; j++ ) {
-            printf( "%d ", matrix5x5[i][j] ) ;
-        }
-        printf( "\n" );
-    }
-
     return 0;
 }
