1b34d2fec43a3f95391459a64641297906c69ad7
Phanthip <phanthiphyphuththwngs@gmail.com>
2024-10-11 15:07:17 +0700

    Lap 9.c

--- Lap 9.c
+++ Lap 9.c
@@ -1,48 +1,41 @@
 #include <stdio.h>
+ #include <string.h>
+ #include <stdlib.h>
+ #include <time.h>
+ 
+ int main() {
+     
+    char CustomerName[ 15 ] , Name[ 10 ] [ 15 ] ;
+    int Quantity[ 10 ] , NumberOfItems , i ;
+    float UnitPrice[ 10 ] , TotalPrice[ 10 ] , TotalAmount ;
 
-int main() {
-    int Number ;
+    printf( "Enter customer name [ QUIT to stop ] : " ) ;
+    scanf( "%s", Customername ) ;
     
-    // รับค่าขนาดของอาเรย์จากผู้ใช้
-    printf( "Enter N :\n" ) ;
-    scanf( "%d" , & Number ) ;
-    
-    int array[ Number ] ;
-    
-    // รับค่าตัวเลขเข้ามาในอาเรย์
-    for( int i = 0 ; i < Number ; i++ ) {
-        printf( "Enter value[ %d ] :\n" , i ) ;
-        scanf( "%d", &array[ i ] ) ;
-    }
-    
-    // แสดงผลลัพธ์
-    printf( "Index:  " ) ;
-    for(int i = 0; i < Number; i++) {
-        printf( "%d  " , i ) ;
-    }
-    printf( "\n Array :  " ) ;
-    
-    for( int i = 0 ; i < Number ; i++ ) {
-        int num = array[ i ] ;
-        int isPrime = 1 ;  // 1 คือจำนวนเฉพาะ
-        
-        if ( num < 2 ) {
-            isPrime = 0 ;
-        } else {
-            for ( int j = 2 ; j <= num / 2 ; j++ ) {
-                if ( num % j == 0 ) {
-                    isPrime = 0 ;  // ไม่เป็นจำนวนเฉพาะ
-                    break ;
-                }
-            }
-        }
-                // ถ้าเป็นจำนวนเฉพาะให้แสดงตัวเลข, ถ้าไม่เป็นให้แสดง #
-        if ( isPrime ) {
-            printf( "%d  " , num) ;
-        } else {
-            printf( "#  " ) ;
-        }
-    }
+     while( strcmp ( CustomerName , "QUIT" ) ! = 0 ) i = 0 ;
+      printf( "Enter name , quantity , and price for item %d [END 0 0] : " , i+1 ) ;
+      scanf( "%s %d %f" , Name[ i ] ,  & Quantity[ i ] , UnitPrice[ i ] ) ;
+     
+     while( strcmp ( Name[ i ] , "END" ) ! = 0 ) { 
+      i++ ;
+      printf( "Enter name , quantity , and price for item %d [END 0 0] : " , i+1 ) ;
+      scanf( "%s %d %f" , Name[ i ] , & Quantity[ i ] , & UnitPrice[ i ] ) ;
+      NumberOfItems = i ;
+      
+     }//end while
 
-    return 0 ;
-}
+ TotalAmount = 0.0 ;
+ for(int i = 0 ; i < NumberOfItems ; i++ ) {
+     TotalPrice[ i ] = Quantity[ i ] * UnitPrice[ i ] ;
+     TotalAmount += TotalPrice[ i ] ;
+       printf( "--------------------------------------------------------------\n" ) ;
+       printf( "%-20s %11s %13s %17s\n" ,  "Name" , "Quantity" , "Unit Price" , "Total Price" ) ;
+       printf( "--------------------------------------------------------------\n" ) ;
+      
+       for( i = 0 ; i < NumberOfItems ; i++ ) ;
+       printf( "%-20s %11d %13.2f %17.2f\n" , Name[ i ] , Quantity[ i ] , UnitPrice[ i ] , TotalPrice[ i ] ) ;
+       printf( "--------------------------------------------------------------\n" )
+       printf( "%52s %11.2f\n" , "TOTAL AMOUNT :" , TotalAmount ) ;
+       printf( "Enter customer name [QUIT to stop] :" ) ;
+       scanf( "%s" , CustomerName ) ;
+ }//end main
