b6e6f07053f3c00e001db7079e11ad74883bde88
Phanthip <phanthiphyphuththwngs@gmail.com>
2024-10-12 20:50:49 +0700

    Lab1.c

--- Lab1.c
+++ Lab1.c
@@ -1,21 +1,14 @@
 #include <stdio.h>
 
 int main() {
-    char Name[50];
-    int Age = 0;
+    char Name[ 50 ] ;
+    int Age = 0 ;
+    printf( "Enter your name: " ) ;
+    scanf( "%s", &Name ) ;
+    printf( "Enter your age: " ) ;
+    scanf( "%d", &Age ) ;
+    printf( "- - - - - -\n" ) ;
+    printf( "Hello %s \n", Name ) ; //TODO: #14 Say hello to user.
+    printf( "Age = %d\n", Age ) ; //TODO: #15 display user their age number plus 1.
     
-    // รับค่าชื่อจากผู้ใช้โดยไม่ขึ้นบรรทัดใหม่
-    printf("Enter your name: ");
-    scanf("%s", Name);
-    
-    // รับค่าอายุจากผู้ใช้โดยไม่ขึ้นบรรทัดใหม่
-    printf("Enter your age: ");
-    scanf("%d", &Age);
-    
-    // แสดงผลตามรูปแบบที่ต้องการ
-    printf(" - - - - - -\n");  // ใส่ขีดขั้นตามต้องการ
-    printf("Hello %s \n", Name);  // ทักทายด้วยชื่อผู้ใช้
-    printf("Age = %d\n", Age + 1);  // แสดงอายุ +1
-    
-    return 0;
-}
+}//end main function
