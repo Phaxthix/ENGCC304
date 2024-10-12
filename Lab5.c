9e9d1ca60e7acbc388cf3ce9c56a04ed9ca08958
Phanthip <phanthiphyphuththwngs@gmail.com>
2024-10-12 20:52:18 +0700

    Lab5.c

--- Lab5.c
+++ Lab5.c
@@ -1,40 +1,32 @@
 #include <stdio.h>
-int main() {
-   
-    int score = 0 ; 
-    printf( "enter score : " ) ;
 
-    if(scanf( "%d", &score ) != 1 ) {
-        printf("Please enter number only.\n") ;
+int main() {
+    
+    int score = 0 ;
+    printf( "Enter score: " ) ;
+    if( scanf ("%d", &score) != 1 ) {
+        printf( "Please enter number only." ) ;
         return 1 ;
-    }
+    }//end if
     
-    
-    if(score >= 80 ) {
-         printf( "A !\n " ) ;
-    }     
-     else if(score >= 75 && score < 80 ) {
-         printf( "B+ !\n" ) ; 
-     }
-     else if(score >= 75 && score < 70 ) {
-         printf( "B !\n" ) ;
-     }
-     else if(score >= 70 && score < 65 ) {
-         printf( "C+ !\n" ) ;
-     }
-     else if(score >= 65 && score < 60 ) {
-         printf( "C !\n" ) ;
-     }
-     else if(score >= 60 && score < 55 ) {
-         printf( "D+ !\n" ) ;
-     } 
-     else if(score >= 55 && score < 50 ) {
-         printf( "D !\n" ) ;
-     }
-     else if( score <=50 ) {
-         printf( "F !\n" ) ;
-     }
-   
-   
+    if( score >= 80 ) {
+        printf( "A !" ) ;
+    } else if( score >= 75 && score < 80 ) {
+        printf( "B+ !" ) ;
+    } else if( score >= 70 && score < 75 ) {
+        printf( "B !" ) ;
+    } else if( score >= 65 && score < 70 ) {
+        printf( "C+ !" ) ;
+    } else if( score >= 60 && score < 65 ) {
+        printf( "C !" ) ;
+    } else if( score >= 55 && score < 60 ) {
+        printf( "D+ !" ) ;
+    } else if( score >= 50 && score < 55 ) {
+        printf( "D !" ) ;
+    } else { 
+        printf( "F !" ) ;
+    }//end if
+
     return 0 ;
-}//end main function
+
+}//end function
